1317f527263bcb260aee187f5c5b3bfc4bd5dde9
sheena21 <mahaksaxenaa@gmail.com>
2021-05-04 17:54:02 +0530

    Project SetUp

---
+++ src/main/java/com/notifier/mailNotifier/MailNotifierApplication.java
@@ -0,0 +1,13 @@
+package com.notifier.mailNotifier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MailNotifierApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MailNotifierApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/notifier/mailNotifier/MailNotifierApplicationTests.java
@@ -0,0 +1,13 @@
+package com.notifier.mailNotifier;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MailNotifierApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
